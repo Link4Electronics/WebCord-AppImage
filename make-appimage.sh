6ba6661d0a3f53f4b7f767be2c0c78f0a499f243
Link4Electronics <link4electronics@gmail.com>
2026-01-20 05:42:23 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -10,7 +10,7 @@ export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
 export ICON=/usr/share/icons/hicolor/512x512/apps/webcord.png
 export DESKTOP=/usr/share/applications/
-export DEPLOY_PULSE=1
+export DEPLOY_PIPEWIRE=1
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
