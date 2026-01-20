c50cd2fd7bf95eecfbdc6f749f483c620e2b5950
Link4Electronics <link4electronics@gmail.com>
2026-01-20 03:01:38 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,6 +13,6 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-make-aur-package --chaotic-aur webcord
+make-aur-package electron39-bin
 
 # If the application needs to be manually built that has to be done down here
