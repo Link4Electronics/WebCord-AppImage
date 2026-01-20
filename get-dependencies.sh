a5f2705546a72eb50bc4a636a0fc1205e5507009
Link4Electronics <link4electronics@gmail.com>
2026-01-20 03:26:07 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -13,13 +13,13 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-if [ "$ARCH" = 'x86_64' ]; then
+#if [ "$ARCH" = 'x86_64' ]; then
 make-aur-package electron39-bin
-else
+#else
 # If the application needs to be manually built that has to be done down here
-git clone https://aur.archlinux.org/electron39-bin.git
-cd electron39-bin
+#git clone https://aur.archlinux.org/electron39-bin.git
+#cd electron39-bin
 # Fix the duplicate aarch64 entry in the PKGBUILD
-sed -i '/^arch=(/ s/\x27aarch64\x27//2' PKGBUILD
-makepkg -si --noconfirm
-fi
+#sed -i '/^arch=(/ s/\x27aarch64\x27//2' PKGBUILD
+#makepkg -si --noconfirm
+#fi
