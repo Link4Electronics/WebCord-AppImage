2a9b219407a69bea44e8332532f7d4e0a92f4ccb
Link4Electronics <link4electronics@gmail.com>
2026-01-20 22:09:44 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,8 +7,7 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm nss \
-    pipewire-jack  \
-    pipewire-pulse
+    pipewire-jack
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
