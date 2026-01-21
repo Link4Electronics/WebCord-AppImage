85cd502a910d300da39d9ff1259d6592cacdcb72
Link4Electronics <link4electronics@gmail.com>
2026-01-21 00:03:04 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -14,15 +14,9 @@ echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-#if [ "$ARCH" = 'x86_64' ]; then
+if [ "$ARCH" = 'x86_64' ]; then
+make-aur-package --chaotic-aur webcord
+else
 make-aur-package electron39-bin
-#else
-#usermod -p '*' nobody
-#chage -E -1 -m 0 -M 99999 -I -1 nobody
-#echo "nobody ALL=(ALL) NOPASSWD: /usr/bin/pacman" >> /etc/sudoers
+fi
 # If the application needs to be manually built that has to be done down here
-#git clone https://aur.archlinux.org/electron39-bin.git
-#chown -R nobody:nobody electron39-bin
-#cd electron39-bin
-#sudo -u nobody makepkg -si --noconfirm
-#fi
