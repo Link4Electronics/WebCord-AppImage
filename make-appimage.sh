9dea53198f2163214150e8f9abd5e22f26c39a66
Link4Electronics <link4electronics@gmail.com>
2026-02-01 04:06:58 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -8,14 +8,14 @@ export ARCH VERSION
 export OUTPATH=./dist
 export ADD_HOOKS="self-updater.bg.hook"
 export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-export ICON=/usr/share/icons/hicolor/512x512/apps/webcord.png
-export DESKTOP=webcord.desktop
+#export ICON=/usr/share/icons/hicolor/512x512/apps/webcord.png
+#export DESKTOP=webcord.desktop
 export DEPLOY_PIPEWIRE=1
 export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/webcord ./AppDir/lib/electron39/
+quick-sharun /usr/bin/webcord
 
 # Additional changes can be done in between here
 
