90521cf3c79aedad7ba53b5165bf13c685da94d3
Link4Electronics <link4electronics@gmail.com>
2026-02-01 04:11:18 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -3,21 +3,36 @@
 set -eu
 
 ARCH=$(uname -m)
+DEB_LINK="https://github.com/SpacingBat3/WebCord/releases/download/latest/webcord_4.12.1_$ARCH.deb"
 
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm nss \
-    pipewire-jack
+    pipewire-jack \
+    pipewire
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
 # Comment this out if you need an AUR package
-if [ "$ARCH" = 'x86_64' ]; then
-make-aur-package --chaotic-aur webcord
-else
-make-aur-package electron39-bin
-make-aur-package webcord
-fi
+#make-aur-package
+
 # If the application needs to be manually built that has to be done down here
+
+echo "Getting app..."
+echo "---------------------------------------------------------------"
+if ! wget --retry-connrefused --tries=30 "$DEB_LINK" -O /tmp/app.deb 2>/tmp/download.log; then
+	cat /tmp/download.log
+	exit 1
+fi
+
+ar xvf /tmp/app.deb
+tar -xvf ./data.tar.gz
+rm -f ./*.gz
+mv -v ./usr ./AppDir
+cp -v ./AppDir/share/applications/webcord.desktop            ./AppDir
+cp -v ./AppDir/share/pixmaps/webcord.png  ./AppDir/.DirIcon
+cp -v ./AppDir/share/pixmaps/webcord.png  ./AppDir
+
+awk -F'/' '/Location:/{print $(NF-1); exit}' /tmp/download.log > ~/version
