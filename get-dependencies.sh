09355cd82726a088c7af7aab43ba86c23f347665
Link4Electronics <link4electronics@gmail.com>
2026-02-12 01:37:44 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,6 +7,8 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
+	nspr           \
+	nss            \
 	pipewire-audio \
     pipewire-jack
 
