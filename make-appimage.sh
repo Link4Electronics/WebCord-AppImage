1f22aeba0cdd8ae22df3a35866194f5ebc92f75c
Link4Electronics <link4electronics@gmail.com>
2026-02-12 01:35:10 -0300

    Update make-appimage.sh

--- make-appimage.sh
+++ make-appimage.sh
@@ -12,7 +12,7 @@ export DEPLOY_OPENGL=1
 export DEPLOY_VULKAN=1
 
 # Deploy dependencies
-quick-sharun /usr/bin/webcord
+quick-sharun ./AppDir/bin/webcord
 
 # Additional changes can be done in between here
 
